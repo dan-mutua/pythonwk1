be3482655c35fc18a2b3a64277179fc292473808
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-09-04 00:46:09 +0300

    sign in users

--- main.py
+++ main.py
@@ -14,3 +14,73 @@ def main():
     if nav_code == 'tt':
       print ("create username")
       created_user_name = input()
+
+
+      print ("create password")
+      created_password = input()
+
+
+      print ("re-enter your password")
+      re_enter_password= input()
+
+
+      while re_enter_password != created_password:
+        print("password did not match")
+        print("enter your password")
+        created_password = input()
+        print("re-enter your password")
+        re_enter_password= input()
+
+
+      else:
+        print("thank you succecfullly created a new account")  
+        print('\n')
+        print ("log in to your account")
+        my_username =input()
+        print("password")
+        my_password=input()
+
+
+      while  my_username != created_user_name or created_password != my_password:
+        print("Invalid log in credentials")  
+        print ("log in to your account")
+        my_username =input()
+        print("password")
+        my_password=input()
+
+
+      else:
+        print("welcome {name}")  
+
+    elif nav_code == 'll':
+      print ("{name} welcome back")
+      print (" enter your username")
+      existing_username=input()
+
+      print ("password")
+      existing_password=input()
+
+      while existing_username != 'user' or existing_password != 'password':
+        print( "wrong user or password")
+        print (" enter your username")
+        existing_username=input()
+
+
+        print ("password")
+        existing_password=input()
+
+
+      else:
+        print(" you successfuly logged in , you can add your password here")  
+
+    elif nav_code == 'uu':
+      break
+    else:
+      print("enter valid code to navigate")  
+
+if __name__ == '__main__':
+  main()        
+
+
+
+
